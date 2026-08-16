825b48e49d43a67e7d5fd6c578521cec61a65549
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-16 05:29:26 -0700

    Create multiplication_table.c

---
+++ multiplication_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    int i;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("\nMultiplication Table of %d\n", number);
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+
+    return 0;
+}
